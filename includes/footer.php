c42b4d02b38b05cecee9a06a9238d9d69f69b228
MuhammadNoumanAli <bzumultan0@gmail.com>
2023-05-06 19:14:36 +0500

    solve slider issue

--- includes/footer.php
+++ includes/footer.php
@@ -141,453 +141,178 @@
     window.RS_MODULES.type = 'compiled';
 
 </script>
-
 <link rel='stylesheet' id='elementor-post-1301-css' href='uploads/elementor/css/post-13010085.css?ver=1674131336'
-
       type='text/css' media='all'/>
-
 <link rel='stylesheet' id='rs-plugin-settings-css'
-
       href='wp-content/plugins/revslider/public/assets/css/rs6b02e.css?ver=6.5.25' type='text/css' media='all'/>
 
-
-
 <script type='text/javascript' src='wp-content/plugins/revslider/public/assets/js/rbtools.min8331.js?ver=6.5.18' defer
-
         async id='tp-tools-js'></script>
-
 <script type='text/javascript' src='wp-content/plugins/revslider/public/assets/js/rs6.minb02e.js?ver=6.5.25' defer async
-
         id='revmin-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/appearae48.js?ver=6.2' id='appear-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/popper-minae48.js?ver=6.2'
-
         id='nextbit-popper-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/bootstrap.minae48.js?ver=6.2'
-
         id='bootstrap-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/custom-adminae48.js?ver=6.2'
-
         id='custom-admin-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.easing.minae48.js?ver=6.2'
-
         id='easing-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.fancyboxae48.js?ver=6.2'
-
         id='fancybox-js'></script>
-
 <script type='text/javascript'
-
         src='wp-content/themes/nextbit/assets/js/jquery.mCustomScrollbar.concat.minae48.js?ver=6.2'
-
         id='mCustomScrollbar-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.mixitupae48.js?ver=6.2'
-
         id='mixitup-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.paroller.minae48.js?ver=6.2'
-
         id='paroller-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/typeitae48.js?ver=6.2' id='typeit-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.magnific-popup.minae48.js?ver=6.2'
-
         id='magnific-popup-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/odometer.minae48.js?ver=6.2'
-
         id='odometer-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/metisMenu.minae48.js?ver=6.2'
-
         id='metisMenu-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/nav-toolae48.js?ver=6.2'
-
         id='nav-tool-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/owlae48.js?ver=6.2' id='owl-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/swiper.minae48.js?ver=6.2'
-
         id='swiper-min-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/lightcaseae48.js?ver=6.2'
-
         id='lightcase-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/backToTopae48.js?ver=6.2'
-
         id='backToTop-js'></script>
-
 <script type='text/javascript' src='includes/js/jquery/ui/core.min3f14.js?ver=1.13.2'
-
         id='jquery-ui-core-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/parallax.minae48.js?ver=6.2'
-
         id='parallax-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/cursor-bundleae48.js?ver=6.2'
-
         id='cursor-bundle-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/jquery.counterup.minae48.js?ver=6.2'
-
         id='counterup-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/waypoints.minae48.js?ver=6.2'
-
         id='waypoints-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/tilt.jquery.minae48.js?ver=6.2'
-
         id='tilt-jquery-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/wowae48.js?ver=6.2' id='wow-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/nextbit-yithae48.js?ver=6.2'
-
         id='nextbit-yith-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/mainae48.js?ver=6.2'
-
         id='nextbit-main-js'></script>
-
 <script type='text/javascript' src='wp-content/themes/nextbit/assets/js/nextbit-customae48.js?ver=6.2'
-
         id='nextbit-custom-js'></script>
-
 <script type='text/javascript' src='includes/js/underscore.mind584.js?ver=1.13.4' id='underscore-js'></script>
-
 <script type='text/javascript' src='includes/js/wp-util.minae48.js?ver=6.2' id='wp-util-js'></script>
 
 <script type='text/javascript' src='wp-content/plugins/elementor/assets/js/webpack.runtime.min695d.js?ver=3.12.0'
-
         id='elementor-webpack-runtime-js'></script>
-
 <script type='text/javascript' src='wp-content/plugins/elementor/assets/js/frontend-modules.min695d.js?ver=3.12.0'
-
         id='elementor-frontend-modules-js'></script>
-
 <script type='text/javascript' src='wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min05da.js?ver=4.0.2'
-
         id='elementor-waypoints-js'></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type='text/javascript' id='elementor-frontend-js-before'>
-
     var elementorFrontendConfig = {
-
         "environmentMode": {"edit": false, "wpPreview": false, "isScriptDebug": false},
-
         "i18n": {
-
             "shareOnFacebook": "Share on Facebook",
-
             "shareOnTwitter": "Share on Twitter",
-
             "pinIt": "Pin it",
-
             "download": "Download",
-
             "downloadImage": "Download image",
-
             "fullscreen": "Fullscreen",
-
             "zoom": "Zoom",
-
             "share": "Share",
-
             "playVideo": "Play Video",
-
             "previous": "Previous",
-
             "next": "Next",
-
             "close": "Close"
-
         },
-
         "is_rtl": false,
-
         "breakpoints": {"xs": 0, "sm": 480, "md": 768, "lg": 1025, "xl": 1440, "xxl": 1600},
-
         "responsive": {
-
             "breakpoints": {
-
                 "mobile": {
-
                     "label": "Mobile Portrait",
-
                     "value": 767,
-
                     "default_value": 767,
-
                     "direction": "max",
-
                     "is_enabled": true
-
                 },
-
                 "mobile_extra": {
-
                     "label": "Mobile Landscape",
-
                     "value": 880,
-
                     "default_value": 880,
-
                     "direction": "max",
-
                     "is_enabled": false
-
                 },
-
                 "tablet": {
-
                     "label": "Tablet Portrait",
-
                     "value": 1024,
-
                     "default_value": 1024,
-
                     "direction": "max",
-
                     "is_enabled": true
-
                 },
-
                 "tablet_extra": {
-
                     "label": "Tablet Landscape",
-
                     "value": 1200,
-
                     "default_value": 1200,
-
                     "direction": "max",
-
                     "is_enabled": false
-
                 },
-
                 "laptop": {
-
                     "label": "Laptop",
-
                     "value": 1366,
-
                     "default_value": 1366,
-
                     "direction": "max",
-
                     "is_enabled": false
-
                 },
-
                 "widescreen": {
-
                     "label": "Widescreen",
-
                     "value": 2400,
-
                     "default_value": 2400,
-
                     "direction": "min",
-
                     "is_enabled": false
-
                 }
-
             }
-
         },
-
         "version": "3.12.0",
-
         "is_static": false,
-
         "experimentalFeatures": {
-
             "e_dom_optimization": true,
-
             "e_optimized_assets_loading": true,
-
             "e_optimized_css_loading": true,
-
             "a11y_improvements": true,
-
             "additional_custom_breakpoints": true,
-
             "landing-pages": true
-
         },
-
         "urls": {"assets": "https:\/\/themexriver.com\/wp\/nextbit\/wp-content\/plugins\/elementor\/assets\/"},
-
         "swiperClass": "swiper-container",
-
         "settings": {"page": [], "editorPreferences": []},
-
         "kit": {
-
             "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
-
             "global_image_lightbox": "yes",
-
             "lightbox_enable_counter": "yes",
-
             "lightbox_enable_fullscreen": "yes",
-
             "lightbox_enable_zoom": "yes",
-
             "lightbox_enable_share": "yes",
-
             "lightbox_title_src": "title",
-
             "lightbox_description_src": "description"
-
         },
-
         "post": {
-
             "id": 15,
-
             "title": "NextBit%20%E2%80%93%20TV%20%26%20Internet%20Provider%20WordPress%20Theme",
-
             "excerpt": "",
-
             "featuredImage": false
-
         }
-
     }; 
-
 </script>
-
 <script type='text/javascript' src='wp-content/plugins/elementor/assets/js/frontend.min695d.js?ver=3.12.0'
-
         id='elementor-frontend-js'></script>
-
 <script type='text/javascript' src='wp-content/plugins/element-helper/assets/js/elh-element8a54.js?ver=1.0.0'
-
         id='element-helper-js'></script>
-
-
-        <script>
-                // $('.logo-container').hide();
-                // console.log($('#iframe-id').contents().find('.logo-container .logo'));
-                // $('#iframe-id').contents().find('#language-selector-container').hide();
-                // var poweredWrapper = $('#iframe-id').contents().find("#language-selector-container").hide();
-                // console.log(poweredWrapper);
-
-                // // $('#iframe-id').ready(function(){
-                // //         $('#iframe-id').contents().find("#language-selector-container").hide();
-                // // });
-                // $(document).ready(function() {
-                //         $('#language-selector-container', frames['iframe-id'].document).hide();
-                //         $('#iframe-id').contents().find("#language-selector-container").hide();
-                // } );
-
-                // $(document).ready(function(){ 
-                //         setTimeout(function () {
-                //                         $('#language-selector-container', frames['iframe-id'].document).hide();
-                //                         $("#iframe-id").contents().find("#language-selector-container").hide();
-                                        
-                //                 },300
-                //         );
-                //         setTimeout(function () {
-                //                 $("#iframe-id").contents().find("#language-selector-container").hide();
-                //                 }, 3000
-                //         );
-                // });
-
-                // $(document).ready(function() {
-                //         $('#language-selector-container', frames['iframe-id'].document).hide();
-                // } );
-
-                // function inIframe() {
-                //         try {
-                //                 return window.self !== window.top;
-                //         } catch (Exception) {
-                //                 return true;
-                //         }
-                // }
-
-                //         if (inIframe()) {
-                //         document.getElementsByTagName('html')[0].classList.add('loaded-in-iframe');
-                //         // Alternatively you could skip the CSS and just hide elements using JavaScript here!.
-                //         }
-
-
-                        $(document).ready(function() {
-                                var windowURL = window.location.href;
-                                if (windowURL.indexOf('noheaderfooter') > -1) {
-                                        $('.footer-container').css('display', 'none');
-                                }
-                                // var frame = document.querySelector("iframe");
-                                // header = frame.contentDocument.querySelector("header");
-                                // header.remove();
-                                // footer = frame.contentDocument.querySelector("footer");
-                                // footer.remove();
-
-                                // var iframe = document.getElementById("iframe-id");
-                                // var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-                                // var style = iframeDocument.createElement("style");
-                                // style.innerHTML = ".logo-container{ display: none; }";
-                                // iframeDocument.head.appendChild(style);
-
-                                // console.log(style);
-
-                                var iframe = document.getElementById("iframe-id");
-                                var element = iframe.contentWindow.document.getElementsByClassName("logo-container");
-                                element.style.display = "none";
-
-                                var iframe = $("#iframe-id").contents();
-                                iframe.find(".logo-container").hide();
-                        });
-
-                        function inIframe() {
-                                try {
-                                        return window.self !== window.top;
-                                } catch(e) {
-                                        return true;
-                                }
-                        }
-
-                        if(inIframe){
-                                // document.getElementsByClassName('logo-container').style.display = "none"; //if you want javascript only
-                                $(".logo-container").hide(); // if you want jquery
-                        }
-
-
-                        (function(){
-                                if (window!=window.top) {
-                                        var noiframe = document.getElementsByClassName("noiframeyall"), 
-                                        body = document.getElementsByTagName('body')[0],
-                                        html = document.getElementsByTagName('html')[0];
-
-                                        for (i=0, len = noiframe.length; i < len; i++){
-                                        noiframe[i].style.display = 'none';
-                                        }
-                                body.className = body.className.replace( /(?:^|\s)noiframe(?!\S)/ , '' );
-                                html.style.overflow = 'hidden';
-                                }
-                        })();
-
-                        
-        </script>
-        
-
+</body>
 </body>
 
 
